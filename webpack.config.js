656f539689fd70f3f71cab2c2ec512eaa198e206
Ryan Lok <rlok.pc@gmail.com>
2022-08-01 14:13:03 +0800

    Set up project files and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,20 @@ module.exports = {
     devServer: {
         static: './dist',
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpeg|jpg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource"
+            },
+        ],
+    }
 }
