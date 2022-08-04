8712bc33bf54dbc13bbcde9cb51ae3dd2f435298
Ryan Lok <rlok.pc@gmail.com>
2022-08-04 15:44:13 +0800

    Add code to create basic sidebar

--- src/DOM.js
+++ src/DOM.js
@@ -1,7 +1,7 @@
 import headerImageFile from './receipt.png'
 let contentContainer = document.querySelector(".content-container")
 
-const createHeader  = (function(){
+const createHeader  = function(){
     const header = document.createElement('div')
     header.classList.add('header')
 
@@ -13,7 +13,18 @@ const createHeader  = (function(){
 
     header.append(headerImage, headerText)
     contentContainer.append(header)
-})
+}
 
-export {createHeader}
+const createSidebar = function() {
+    const sidebarOuter = document.createElement('div')
+    sidebarOuter.classList.add('sidebar', 'outer')
+
+    const sideBarInner = document.createElement('div')
+    sideBarInner.classList.add('sidebar', 'inner')
+
+    sidebarOuter.append(sideBarInner)
+    contentContainer.append(sidebarOuter)
+}
+
+export {createHeader, createSidebar}
 
