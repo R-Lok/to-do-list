c78b128b7c21c6bba6a443dcd26da772ed21ba9d
Ryan Lok <rlok.pc@gmail.com>
2022-08-23 18:35:15 +0800

    Create basic sidebar and header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import css from "./style.css"
-import {createHeader} from "./DOM"
+import {createHeader, createSidebar} from "./DOM"
+import { add } from "date-fns"
 
+createHeader()
+createSidebar()
+
+let projects = {
+    list: {},
+    add: function(projectName) {
+        projects.list[projectName] = new project(projectName)
+    }
+}
+
+
+
+class project {
+    constructor(projectName) {
+        this.projectName = projectName
+        this.toDoItems = []
+    }
+    addToDoItem(title, description, dueDate) {
+        this.toDoItems.push(new toDoItem(title, description, dueDate))
+    }
+}
+
+class toDoItem {
+    constructor(title, description, dueDate) {
+        this.title = title
+        this.description = description
+        this.dueDate = dueDate
+    }
+}
+
+projects.add('hi')
+projects.list.hi.addToDoItem('buy food', 'go shop at walmart', 'Nov 16')
+console.log(projects.list.hi.toDoItems)
+
+console.log(projects)
 
 console.log('test')
-createHeader();
